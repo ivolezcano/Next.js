e8cf0eb8ba2264e6e9a5b23e758ff9934652e91f
Ivan Lezcano <lezcamati@gmail.com>
2024-02-19 14:15:30 -0300

    add user details

--- first_project/nextjs_firstproject/app/users/[id]/page.jsx
+++ first_project/nextjs_firstproject/app/users/[id]/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+async function getUser(id) {
+  const res = await fetch(`https://reqres.in/api/users/${id}`)
+  const data = await res.json()
+  console.log(data)
+  return data.data
+}
 
-function usersPage() {
+async function usersPage({ params }) {
+
+  const user = await getUser(params.id)
+  
+
   return (
     <div>
       <h1>User details</h1>
+      <div>
+        <img src={user.avatar} />
+        <div>
+          <h3>{user.id} {user.first_name} {user.last_name}</h3>
+          <p>{user.email}</p>
+        </div>
+      </div>
     </div>
   )
 }
