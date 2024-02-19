84ac01eece99262937358d9afba24a654041365f
Ivan Lezcano <lezcamati@gmail.com>
2024-02-18 23:47:45 -0300

    add fetch data

--- first_project/nextjs_firstproject/app/page.jsx
+++ first_project/nextjs_firstproject/app/page.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-function IndexPage() {
+async function fetchUsers() {
+  const res = await fetch('https://reqres.in/api/users')
+  const data = await res.json()
+  return data.data
+}
+
+async function IndexPage() {
+  const users = await fetchUsers()
+  console.log(users)
   return (
     <div>
       <h1>Index Page</h1>
+      <div>
+        {JSON.stringify(users)}
+      </div>
     </div>
   )
 }
